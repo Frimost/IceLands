a505c1aefd3033e0cdbe03d4ec2b2b078712bb75
Frimost <Frimost@gamerlive.cl>
2011-06-26 22:42:45 -0400

    Core/Arena: Remove exit announce.

--- src/server/game/Battlegrounds/BattlegroundQueue.cpp
+++ src/server/game/Battlegrounds/BattlegroundQueue.cpp
@@ -351,13 +351,14 @@ void BattlegroundQueue::RemovePlayer(const uint64& guid, bool decreaseInvitedCou
     // remove player queue info
     m_QueuedPlayers.erase(itr);
 
-    // announce to world if arena team left queue for rated match, show only once
+    /* // announce to world if arena team left queue for rated match, show only once
     if (group->ArenaType && group->IsRated && group->Players.empty() && sWorld->getBoolConfig(CONFIG_ARENA_QUEUE_ANNOUNCER_ENABLE))
     {
         ArenaTeam *Team = sArenaTeamMgr->GetArenaTeamById(group->ArenaTeamId);
         if (Team)
             sWorld->SendWorldText(LANG_ARENA_QUEUE_ANNOUNCE_WORLD_EXIT, Team->GetName().c_str(), group->ArenaType, group->ArenaType, group->ArenaTeamRating);
     }
+	*/
 
     //if player leaves queue and he is invited to rated arena match, then he have to lose
     if (group->IsInvitedToBGInstanceGUID && group->IsRated && decreaseInvitedCount)
