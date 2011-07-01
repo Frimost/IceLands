248a2ba42308ba6a38995dce1dee91265fe9736f
Metalkat <eduardo.u24@gmail.com>
2011-07-01 11:58:51 -0400

    Adding -autobroadcast- del primero de julio del 2011.

---
+++ sql/IceLandsUpdates/autobroadcast-01-07-2011.sql
@@ -0,0 +1,5 @@
+INSERT INTO `autobroadcast` (`id`, `text`) VALUES (1, 'Si conoces algún error del juego reportalo en el foro [> reporte de errores] para que podamos arreglarlo :).');
+INSERT INTO `autobroadcast` (`id`, `text`) VALUES (2, 'Entérate de todos los cambios, mejoras y novedades del servidor en nuestro nuevo sitio web! ');
+INSERT INTO `autobroadcast` (`id`, `text`) VALUES (3, 'Los ticket que no contengan el problema concreto o no contengan información con la cuales les podamos ayudar serán cerrados automáticamente sin respuesta (Ticket sin información: “GM ayuda” “ALo?” “alguien”).');
+INSERT INTO `autobroadcast` (`id`, `text`) VALUES (4, 'Los GMs no dan soporte ni responden dudas por canales públicos, si quieres contactar a un GM envía un ticket y espera a que algún miembro del Staff te contacte.');
+INSERT INTO `autobroadcast` (`id`, `text`) VALUES (5, 'Spells Prohibidas:  Sacred cleansy & Improved icy talons. (más info en el foro) - No arriesgues Ban por una simple Spell.');
