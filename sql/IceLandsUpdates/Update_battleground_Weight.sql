1dc8ab184133a75f885897365cefc14747491ef7
Ramp4 <cesgbla@gmail.com>
2011-07-01 02:30:50 -0400

    DB: update bg
    - disminuye el % de que salga AV en random bg

---
+++ sql/IceLandsUpdates/Update_battleground_Weight.sql
@@ -0,0 +1,6 @@
+-- aumentar el % en RB para todas menos AV
+UPDATE `battleground_template` SET `Weight`= 3 WHERE `id` = 2; -- warsong
+UPDATE `battleground_template` SET `Weight`= 3 WHERE `id` = 3; -- arathi
+UPDATE `battleground_template` SET `Weight`= 3 WHERE `id` = 7; -- eye
+UPDATE `battleground_template` SET `Weight`= 3 WHERE `id` = 9; -- sota
+UPDATE `battleground_template` SET `Weight`= 3 WHERE `id` = 30; -- ioc
