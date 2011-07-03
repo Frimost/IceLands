e0f71a81a84a0ec8827d075aa63112a1b81a8710
Metalkat <eduardo.u24@gmail.com>
2011-07-03 15:03:08 -0400

    Agrego game_event.sql con Darkmoon para 2011

---
+++ sql/IceLandsUpdates/game_event.sql
@@ -0,0 +1,14 @@
+-- Actualizando Darkmoon para 2011
+UPDATE game_event SET start_time = '2011-06-21 00:01:00' WHERE eventEntry=1; -- Midsummer Fire Festival
+UPDATE game_event SET start_time = '2011-12-15 06:00:00' WHERE eventEntry=2; -- Winter Veil
+UPDATE game_event SET start_time = '2011-02-06 00:01:00' WHERE eventEntry=3; -- Darkmoon Faire - Terrokkar Forest
+UPDATE game_event SET start_time = '2011-03-06 00:01:00' WHERE eventEntry=4; -- Darkmoon Faire - Elwynn Forest
+UPDATE game_event SET start_time = '2011-01-02 00:01:00' WHERE eventEntry=5; -- Darkmoon Faire - Mulgore
+UPDATE game_event SET start_time = '2011-01-23 00:01:00' WHERE eventEntry=7; -- Lunar Festival
+UPDATE game_event SET start_time = '2011-02-06 00:01:00' WHERE eventEntry=8; -- Love is in the Air
+UPDATE game_event SET start_time = '2011-04-24 00:01:00' WHERE eventEntry=9; -- Noblegarden
+UPDATE game_event SET start_time = '2011-05-01 00:01:00' WHERE eventEntry=10; -- Childrens Week
+UPDATE game_event SET start_time = '2011-09-06 00:01:00' WHERE eventEntry=11; -- Harvest Festival
+UPDATE game_event SET start_time = '2011-10-18 01:00:00' WHERE eventEntry=12; -- Hallow's End
+UPDATE game_event SET start_time = '2011-03-03 00:01:00' WHERE eventEntry=23; -- Darkmoon Faire Building - Elwynn Forest
+UPDATE game_event SET start_time = '2011-09-19 00:01:00' WHERE eventEntry=52; -- Pirates Day
